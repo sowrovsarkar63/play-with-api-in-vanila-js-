3361e4671aac9bbd1588d1d70e49fec651cba82c
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-08-27 21:59:23 +0600

    load post from api in the web page

--- post.js
+++ post.js
@@ -1,1 +1,19 @@
-console.log("post working ");
+function getPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => loadPost(data));
+}
+
+function loadPost(data) {
+  const postContainer = document.getElementById("posts");
+  for (const posts of data) {
+    const div = document.createElement("div");
+    div.classList.add("post");
+    div.innerHTML = `
+    <h1>${posts.title}</h1>
+    <p>${posts.body}</p>
+    `;
+    postContainer.appendChild(div);
+  }
+}
+getPost();
